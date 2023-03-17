a926bc00226621c6882e8df15f18f84304cd960a
Dan Chaltiel <dan.chaltiel@gmail.com>
2023-03-17 20:21:42 +0100

    fixed namespace and description

--- R/autoimport-package.R
+++ R/autoimport-package.R
@@ -1,7 +1,11 @@
-#' @keywords internal
-"_PACKAGE"
+utils::globalVariables(c(".", "x", "y", "fun", "pkg", "value", "values", "ind",
+                         "tmp", "label", "action", "token", "text",
+                         "fun_imported", "pkg_n_imports", "pkg_in_desc"))
 
+#' @keywords internal
+#' @name autoimport-package
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
+#' @importFrom dplyr %>%
 ## usethis namespace: end
-NULL
+"_PACKAGE"
