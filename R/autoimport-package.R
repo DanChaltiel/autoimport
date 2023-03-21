ee6fced24c21662dea3f3f919e795de44439233a
Dan Chaltiel <dan.chaltiel@gmail.com>
2023-03-21 19:02:35 +0100

    fixed check warning about doc aliases

--- R/autoimport-package.R
+++ R/autoimport-package.R
@@ -1,11 +1,11 @@
 utils::globalVariables(c(".", "x", "y", "fun", "pkg", "value", "values", "ind",
-                         "tmp", "label", "action", "token", "text",
+                         "tmp", "label", "action", "token", "text", "what", "from",
                          "fun_imported", "pkg_n_imports", "pkg_in_desc",
                          "pref_pkg", "package", "pkg_bak"))
 
 #' @keywords internal
 #' @name autoimport-package
-#' @rdname autoimport-package
+#' @aliases autoimport-package
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
 #' @importFrom dplyr %>%
