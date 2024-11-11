ec87d9585bc4cfb616081d5b88c5eea68967df16
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2024-11-11 20:24:41 +0100

    globalVariables

--- R/autoimport-package.R
+++ R/autoimport-package.R
@@ -3,7 +3,7 @@ utils::globalVariables(c(".", "x", "y", "fun", "pkg", "value", "values", "ind",
                          "fun_imported", "pkg_n_imports", "pkg_in_desc",
                          "pref_pkg", "package", "pkg_bak", "cache_dir", "defined_in_importlist",
                          "details", "fun_already_imported", "fun_is_base", "fun_is_inner",
-                         "fun_is_private", "operator", "sessionInfo", "source_fun"))
+                         "fun_is_private", "operator", "sessionInfo", "source_fun", "pkg_str"))
 
 # x="cache_dir defined_in_importlist details fun_already_imported"
 # str_split_1(x, "\\s+") %>% cat(sep='", "')
